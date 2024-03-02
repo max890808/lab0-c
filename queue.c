d0df781ae078f7589669bb910b2fbe3d916e271a
Kevin <N26124361@gs.ncku.edu.tw>
2024-03-02 16:18:29 +0800

    Implement q_remove_tail function
    Use list_del() remove tail,
    and use strncpy() copy the tail element value to sp.

--- queue.c
+++ queue.c
@@ -102,7 +102,17 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *last_element = list_last_entry(head, element_t, list);
+
+    list_del(&last_element->list);
+
+    if (sp) {
+        strncpy(sp, last_element->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return last_element;
 }
 
 /* Return number of elements in queue */
