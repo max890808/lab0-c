ab45163a6eeba7270d538000a1178c59cf7a505b
Kevin <N26124361@gs.ncku.edu.tw>
2024-03-05 19:32:22 +0800

    Implement q_merge function

--- queue.c
+++ queue.c
@@ -344,5 +344,16 @@ int q_descend(struct list_head *head)
 int q_merge(struct list_head *head, bool descend)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
-    return 0;
+    if (!head || list_empty(head))
+        return 0;
+    queue_contex_t *tmp = list_entry(head->next, queue_contex_t, chain);
+    struct list_head *ptr = head->next->next;
+    while (ptr != head) {
+        queue_contex_t *tmp2 = list_entry(ptr, queue_contex_t, chain);
+        list_splice_init(tmp2->q, tmp->q);
+        ptr = ptr->next;
+    }
+    q_sort(tmp->q, descend);
+
+    return q_size(tmp->q);
 }
